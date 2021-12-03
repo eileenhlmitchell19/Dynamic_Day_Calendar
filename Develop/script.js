e787e14ff5e69bee724c347bfcf0956c72f2b07f
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2021-12-02 22:44:58 -0800

    code includes fixes to JS. Originally had created entire code base for static test, had to redo to include JS dynamic text. Includes columns and a button beneath the button.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,6 @@
+var businessHours = [9,10,11,12,13,14,15,16,17];
+var currentHour = moment().hour();
+
 var timeDate = moment();
 
 setCurrentTime();
@@ -22,4 +25,63 @@ function setCurrentTime() {
     let clockEl = $("#DateTime");
     clockEl.text(timeDate.format('MMMM Do YYYY, h:mm:ss a'));
 } 
-//-------------------------------------------
+
+function displayHours(){
+    
+    for( let index=0; index < businessHours.length; index++){
+        
+        
+        let row = $("<div class='row'>");
+        let col1 = $("<div class='col-sm-2'>");
+
+        let workHours = businessHours[index]+ " A.M.";
+
+        if(businessHours[index] >= 12){
+            workHours = businessHours[index] + " P.M.";
+            if(businessHours[index] >= 13){
+                workHours = businessHours[index] - 12 + " P.M";
+            }
+        }
+
+        // col1.append(workHours)
+
+        // if(currentHour > businessHours[index]){
+        //     textArea.addClass("past");
+        // }
+    
+
+        let savedText = localStorage.getItem("textarea"+index );
+     
+        let textArea = $("<textarea>");
+        textArea.attr('id', 'textarea' + index);
+        textArea.addClass('form-control');
+        textArea.text(savedText)
+
+
+
+        let col2 = $("<div class='col-sm-8'>");
+        col2.append(textArea);
+
+
+        let button = $("<button>");
+        button.addClass("saveBtn");
+        button.text("Eileen");
+        let col3 = $("<div class='col-sm-2'>");
+        col3.append(button);
+
+        row.append(col1, col2, col3)
+        $("#planner").append(row);
+    }
+
+
+}
+
+
+displayHours();
+
+$(".saveBtn").on("click", function(){
+    for(let i = 0; i < businessHours.length; i++){
+        let getTxtArea = $("#textarea" + i).val()
+        localStorage.setItem("textarea"+i, getTxtArea);
+    }
+})
